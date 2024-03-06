d3a48adc1fa186078c3de79de98d92dfe0d17ad4
resuadam2 <resuadam2@gmail.com>
2024-03-06 13:45:39 +0100

    adding toggle button to appbar

--- app/src/main/java/com/example/dadocompose/MainScreen.kt
+++ app/src/main/java/com/example/dadocompose/MainScreen.kt
@@ -9,8 +9,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconToggleButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -20,10 +24,13 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -41,6 +48,9 @@ class DadoViewModel : ViewModel() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
+    var imageFlag by rememberSaveable {
+        mutableStateOf(false)
+    }
     Scaffold (
         Modifier.fillMaxSize(),
         topBar = { TopAppBar(
@@ -48,6 +58,14 @@ fun MainScreen() {
                 containerColor = MaterialTheme.colorScheme.primaryContainer,
                 titleContentColor = MaterialTheme.colorScheme.primary,
             ),
+
+            // TODO modificar el icono del toggle para que se vea cuándo está activado o desactivado
+            actions = {
+                      IconToggleButton(checked = imageFlag, onCheckedChange = {imageFlag = !imageFlag},
+                      ) {
+                        Icon(imageVector = Icons.Default.Star, contentDescription = "Toggle Icon")
+                      }
+            },
             title = { Text(text = "DadoCompose")})},
     ) {
         Box(Modifier.padding(it)) {
@@ -57,7 +75,7 @@ fun MainScreen() {
                 horizontalAlignment = Alignment.CenterHorizontally,
             ){
                 val viewModel: DadoViewModel = viewModel()
-                LanzarDado(viewModel.resultadoActual) {
+                LanzarDado(viewModel.resultadoActual, imageFlag) {
                     viewModel.nuevoResultado()
                 }
             }
@@ -66,27 +84,31 @@ fun MainScreen() {
 }
 
 @Composable
-fun LanzarDado(resultadoActual: Int, nuevoResultado: () -> Unit) {
+fun LanzarDado(resultadoActual: Int, image: Boolean, nuevoResultado: () -> Unit) {
     Row {
         Button(onClick = nuevoResultado) {
             Text(text = "Lanzar Dado")
         }
     }
     Row {
-        Text(text = "Resultado: $resultadoActual")
+        if (image) {
+            Image(
+                painter = painterResource(
+                    id = when (resultadoActual) {
+                        1 -> R.drawable.dice_1
+                        2 -> R.drawable.dice_2
+                        3 -> R.drawable.dice_3
+                        4 -> R.drawable.dice_4
+                        5 -> R.drawable.dice_5
+                        6 -> R.drawable.dice_6
+                        else -> R.drawable.empty_dice
+                    }
+                ),
+                contentDescription = "Dado"
+            )
+        } else {
+            Text(text = "Resultado: $resultadoActual")
+        }
     }
-    Row {
-        Image(
-            painter = painterResource(id = when (resultadoActual) {
-                1 -> R.drawable.dice_1
-                2 -> R.drawable.dice_2
-                3 -> R.drawable.dice_3
-                4 -> R.drawable.dice_4
-                5 -> R.drawable.dice_5
-                6 -> R.drawable.dice_6
-                else -> R.drawable.empty_dice
-            }),
-            contentDescription = "Dado"
-        )
+
     }
-}
